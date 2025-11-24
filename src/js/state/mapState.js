267d8c478731d68503c1df4048ac054741c3ffea
BotBY <BotBY@users.noreply.github.com>
2025-11-23 20:17:17 -0800

    Add files via upload

--- src/js/state/mapState.js
+++ src/js/state/mapState.js
@@ -1,29 +1,33 @@
-import {observable,action, reaction} from 'mobx'
+import { observable, action, reaction } from 'mobx'
 import L from 'leaflet'
 import 'leaflet.gridlayer.googlemutant'
-import { Lmock , Lfootprint, Lmyposition} from 'component/LMapMarker'
+import { Lmock, Lfootprint, Lmyposition } from 'component/LMapMarker'
 import authConstnat from 'constant/auth'
 import mapConstant from 'constant/map'
 import api from 'api';
 
 
-class Map{
+class Map {
     map
     mockGroup
     mypositionGroup
     footprintGroup
-    @observable mockposition 
+    @observable mockposition
 
 
-    init= (map)=>{
+    init = (map) => {
         this.map = map
         // Set default Style
-        L.gridLayer.googleMutant({
-            type:'roadmap'  // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
-        }).addTo(this.map)
+        // L.gridLayer.googleMutant({
+        //     type:'roadmap'  // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
+        // }).addTo(this.map)
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
 
         this.map.removeControl(this.map.zoomControl)
-        
+
 
         // init layerGroup
         this.mockGroup = L.layerGroup().addTo(this.map)
@@ -31,69 +35,69 @@ class Map{
         this.footprintGroup = L.layerGroup().addTo(this.map)
     }
 
-    @action mockTo =(position)=>{
+    @action mockTo = (position) => {
         this.mockposition = position
         this.mockGroup.clearLayers()
 
-        L.marker(position,{icon:Lmock,zIndexOffset:1000})
+        L.marker(position, { icon: Lmock, zIndexOffset: 1000 })
             .addTo(this.mockGroup)
         this.mockGroup.addTo(this.map)
     }
 
-    @action setMyPosition=(position,zoomsize)=>{
-        this.map.setView(position,zoomsize)
+    @action setMyPosition = (position, zoomsize) => {
+        this.map.setView(position, zoomsize)
         this.mockTo(position)
 
         this.mypositionGroup.clearLayers()
-        L.marker(position,{icon:Lmyposition,zIndexOffset:10})
+        L.marker(position, { icon: Lmyposition, zIndexOffset: 10 })
             .addTo(this.mypositionGroup)
         this.mypositionGroup.addTo(this.map)
     }
 
-    setFootprintbyRequest = (id)=>{
+    setFootprintbyRequest = (id) => {
         api.footprintRequest({
             id: id,
-            CSRF_TOKEN : authConstnat.CSRF_TOKEN
+            CSRF_TOKEN: authConstnat.CSRF_TOKEN
         })
-        .then( footprints=>{
-            if(footprints.length){
-                let latest = footprints.slice(-1)[0]
-                this.map.setView(latest, mapConstant.DistrictZoomSize)
+            .then(footprints => {
+                if (footprints.length) {
+                    let latest = footprints.slice(-1)[0]
+                    this.map.setView(latest, mapConstant.DistrictZoomSize)
 
-                footprints.map((position)=>{
-                    L.marker(position,{icon: Lfootprint,zIndexOffset:2000}).addTo(this.footprintGroup)
-                })
-                this.footprintGroup.addTo(this.map)
-            }
-        })
+                    footprints.map((position) => {
+                        L.marker(position, { icon: Lfootprint, zIndexOffset: 2000 }).addTo(this.footprintGroup)
+                    })
+                    this.footprintGroup.addTo(this.map)
+                }
+            })
     }
 
-    setFootprint(position){
+    setFootprint(position) {
         this.map.setView(position, mapConstant.StreetZoomSize)
-        L.marker(position,{icon: Lfootprint}).addTo(this.footprintGroup)          
+        L.marker(position, { icon: Lfootprint }).addTo(this.footprintGroup)
         this.footprintGroup.addTo(this.map)
     }
 
 
-    clearFootprint= ()=>{
+    clearFootprint = () => {
         this.footprintGroup.clearLayers()
         this.footprintGroup.addTo(this.map)
     }
 
 
-    registClickMock = ()=>{
+    registClickMock = () => {
         this.map.addEventListener('click', this.onMapClick)
     }
 
-    unregistClickMock = ()=>{
+    unregistClickMock = () => {
         this.map.removeEventListener('click', this.onMapClick)
     }
 
 
-    onMapClick =(e)=>{
+    onMapClick = (e) => {
         const position = e.latlng
         this.mockTo(position)
-    }   
+    }
 
 
 
